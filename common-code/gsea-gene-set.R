cba8c7023ccfcf69de2769cc33e534967822d89b
jimmy <chengziqi@whu.edu.cn>
2023-04-10 22:17:51 +0800

    Add some genes to do GSEA

---
+++ common-code/gsea-gene-set.R
@@ -0,0 +1,4 @@
+5 resident markers (RUNX3, NR4A1, CD69, CXCR6, and NR4A3),
+7 cytotoxicity associated genes (PRF1, IFNG, GNLY, NKG7, GZMB, GZMA, CST7, and TNFSF10),
+5 exhausted markers (CTLA4, HAVCR2, LAG3, PDCD1, and TIGIT)
+6 costimulatory molecular genes (ICOS, CD226, TNFRSF14, TNFRSF25, TNFRSF9, and CD28)
